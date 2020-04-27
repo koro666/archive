6ed55533d523d48af1c26260d0acfc4fde48616f
Koro <koro@kzxiv.net>
2020-04-26 20:03:30 -0400

    Space out editor script a little.

--- static/editor.js
+++ static/editor.js
@@ -2,15 +2,30 @@ $(document).ready(function()
 {
 	$('#copy').click(function(e)
 	{
-		let buffer = $('#buffer');
-		let parent = buffer.parent();
-		buffer.val('');
-		$('input[name=ids]:checked').next('a').each(function(i, o) { buffer.val(function(i, text) { return text + o.href + '\n'; }); });
-		parent.removeClass('hidden');
-		buffer.select();
-		document.execCommand('copy');
-		parent.addClass('hidden');
-		alert('Links were copied to the clipboard.');
+		let links = $('input[name=ids]:checked').next('a');
+		if (links.length > 0)
+		{
+			let buffer = $('#buffer');
+			let parent = buffer.parent();
+
+			buffer.val('');
+			links.each(function(_, a)
+			{
+				buffer.val(function(_, text)
+				{
+					return text + a.href + '\n';
+				});
+			});
+
+			parent.removeClass('hidden');
+			buffer.select();
+			let copied = document.execCommand('copy');
+			parent.addClass('hidden');
+
+			if (copied)
+				alert('Links were copied to the clipboard.');
+		}
+
 		e.preventDefault();
 	});
 });
